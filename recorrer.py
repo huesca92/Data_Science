fed98a4af8b6ec58b3f1e8ba4ea3c068b25ff915
huesca92 <huesca_92@hotmail.com>
2021-05-11 20:53:10 -0500

    Codigos de curso python basico

---
+++ recorrer.py
@@ -0,0 +1,11 @@
+def run():
+    # nombre = input('Escribe tu nombre: ')
+    # for i in nombre:
+    #     print(i)
+
+    frase = input('Escribe una frase: ')
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ == '__main__':
+    run()
