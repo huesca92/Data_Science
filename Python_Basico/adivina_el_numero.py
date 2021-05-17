ed53ed23bdcd7675dbe9ca4ded5c23c252129c12
huesca92 <huesca_92@hotmail.com>
2021-05-16 22:30:46 -0500

    Organizando los archivos por curso

---
+++ Python_Basico/adivina_el_numero.py
@@ -0,0 +1,18 @@
+import random
+
+def run():
+    numero_aleatorio = random.randint(1,100)
+    numero = int(input('Elige un numero del 1 al 100: '))
+
+    while numero != numero_aleatorio:
+        if numero < numero_aleatorio:
+            print('Elige un numero mas grande')
+        else:
+            print('Elige un numero mas pequeño')
+        numero = int(input('Elige otro numero: '))
+    print('Ganaste!!!!')
+
+if __name__ == '__main__':
+    run()
+
+
---
+++ Python_Basico/conversor_dolares.py
@@ -0,0 +1,4 @@
+dolares = float(input("¿Cuantos dolares tienes?: "))
+valor_dolar=21
+pesos =round(dolares * valor_dolar,2)
+print("Tienes $" + str(pesos)+ " pesos MX")
